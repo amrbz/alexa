5a194656c5339c551bba41722827bfa07f054738
Aleksandr Pantiukhin <sanyapantyukhin@gmail.com>
2019-11-04 14:53:09 +0300

    add sources for arduini and Alexa

---
+++ arduino/app.ino
@@ -0,0 +1,72 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+ 
+const char* ssid = "HUAWEI-vg4N";
+const char* password =  "cs2Hdmn7";
+
+int pin_state = 0;
+
+#define LED_PIN 3
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+  delay(4000);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+  pinMode(LED_PIN, OUTPUT);
+ 
+}
+ 
+void loop() {
+ 
+  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection 
+status
+ 
+    HTTPClient http;
+ 
+    http.begin("http://167.172.163.38/api/v1/alexa"); //Specify the URL
+    int httpCode = http.GET();                                        
+//Make the request
+ 
+    if (httpCode > 0) { //Check for the returning code
+ 
+        String payload = http.getString();
+        Serial.print("Code -> ");
+        Serial.println(httpCode);
+        Serial.print("Payload -> ");
+        Serial.println(payload);
+        Serial.println("");
+        pin_state = payload.toInt();
+
+        if (pin_state == 1)
+        {
+            digitalWrite(LED_PIN, HIGH);
+        }
+        else if (pin_state == 0)
+        {
+            digitalWrite(LED_PIN, LOW);
+        }
+
+        else
+        {
+            Serial.println("Wrong pin state!");
+        }
+        }
+ 
+    else {
+      Serial.println("Error on HTTP request");
+    }
+ 
+    http.end(); //Free the resources
+  }
+ 
+  delay(1000);
+ 
+}
